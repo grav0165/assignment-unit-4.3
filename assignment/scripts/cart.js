e2c465453c8209316092bea96fa6fa45a1612528
Aubrey Graves <grav0165@gmail.com>
2023-05-23 15:28:08 -0500

    created first two functions, debugging array issues

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,8 +2,20 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+let item;
 
 
+function addItem(item) {
+    basket.push(item);
+    return true;
+}
+
+function listItems(basket) {
+    for (i=0; i<basket.length; i++) {
+        console.log(basket[i]);
+    }
+}
 
 
 
